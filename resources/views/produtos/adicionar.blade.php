4d671398cbb7ffb3fa80ebf5b0a878682d5eda9c
Gustavo Doria <gustavogomesdoria@outlook.com>
2020-10-06 23:32:32 -0300

    produto

---
+++ resources/views/produtos/adicionar.blade.php
@@ -0,0 +1,20 @@
+@extends('produtos.layout.site')
+
+@section ('titulo', 'Adicionar Produto')
+
+@section('conteudo')
+
+<div class="container">
+    <h1 style="text-align: center"><b><i>Adcionar Produtos</i></b></h1>
+    <div class="row">
+    <form action="{{ route('produto.salvar') }}" method="post" enctype="multipart/form-data" >
+        @csrf       
+       
+        @include('produtos.form')
+    
+        <br>
+        <button type="submit" class="btn btn-success"> Salvar </button>
+    </form>
+    </div>
+</div>
+@endsection()
