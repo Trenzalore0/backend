c7ba61073e945bf86107664b34fa4c95bc04677d
kleydson <kleydson122@gmail.com>
2020-10-12 15:17:49 -0300

    S

--- database/migrations/2020_09_29_143603_create_enderecos_table.php
+++ database/migrations/2020_09_29_143603_create_enderecos_table.php
@@ -6,36 +6,36 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateEnderecosTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('enderecos', function (Blueprint $table) {
-            $table->id();
-            $table->string('rua');
-            $table->integer('numero');
-            $table->string('bairro');
-            $table->string('cep');
-            $table->unsignedBigInteger('cd_uf');
-            $table->foreign('cd_uf')->references('id')->on('ufs');
-            $table->string('referencia');
-            $table->string('complemento');
-            $table->unsignedBigInteger('cd_cliente');
-            $table->foreign('cd_cliente')->references('id')->on('clientes');
-            $table->timestamps();
-        });
-    }
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('enderecos', function (Blueprint $table) {
+      $table->id();
+      $table->string('rua');
+      $table->integer('numero');
+      $table->string('bairro');
+      $table->string('cep');
+      $table->unsignedBigInteger('cd_uf');
+      $table->foreign('cd_uf')->references('id')->on('ufs');
+      $table->string('referencia');
+      $table->string('complemento');
+      $table->unsignedBigInteger('cd_cliente');
+      $table->foreign('cd_cliente')->references('id')->on('clientes');
+      $table->timestamps();
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('enderecos');
-    }
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('enderecos');
+  }
 }
