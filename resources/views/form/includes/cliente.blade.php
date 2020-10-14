25571422987a1d14477f14ee2a00d98f5d05137d
Kleydson Barros Vieira <Kleydson122@gmail.com>
2020-10-13 22:17:15 -0300

    users

--- resources/views/form/includes/cliente.blade.php
+++ resources/views/form/includes/cliente.blade.php
@@ -5,33 +5,35 @@
 </div>
 <div class="form-group col-6">
   <label for="email">Email</label>
-  <input type="text" class="form-control" id="email" name="email">
+  <input type="text" class="form-control" id="email" name="email"
+    value="{{ $dados->email ?? '' }}">
 </div>
 <div class="form-group col-6">
   <label for="rg">RG</label>
-  <input type="text" class="form-control" id="rg" name="rg">
+  <input type="text" class="form-control" id="rg" name="rg"
+    value="{{ $dados->rg ?? '' }}">
 </div>
 <div class="form-group col-6">
   <label for="cpf">CPF</label>
-  <input type="text" class="form-control" id="cpf" name="cpf">
+  <input type="text" class="form-control" id="cpf" name="cpf"
+    value="{{ $dados->cpf ?? '' }}">
 </div>
 <div class="form-group col-6">
   <label for="data_de_nascimento">Data de Nascimento</label>
-  <input type="date" class="form-control" id="data_de_nascimento" name="data_de_nascimento">
+  <input type="date" class="form-control" id="data_de_nascimento" name="data_de_nascimento"
+    value="{{ $dados->data_de_nascimento ?? '' }}">
 </div>
 <div class="form-group col-6">
   <label for="genero">Genero</label>
-  <input type="text" class="form-control" id="genero" name="genero">
+  <input type="text" class="form-control" id="genero" name="genero"
+    value="{{ $dados->genero ?? '' }}">
 </div>
 <div class="form-group col-6">
   <label for="login">Login</label>
-  <input type="text" class="form-control" id="login" name="login">
+  <input type="text" class="form-control" id="login" name="login"
+    value="{{ $dados->login ?? '' }}">
 </div>
 <div class="form-group col-6">
   <label for="senha">Senha</label>
-  <input type="text" class="form-control" id="senha" name="senha">
-</div>
-<div class="form-group col-6">
-  <label for="cd_login">Cd login</label>
-  <input type="text" class="form-control" id="cd_login" name="cd_login">
+  <input type="password" class="form-control" id="senha" name="senha">
 </div>
