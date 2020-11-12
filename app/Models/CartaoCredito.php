58544cc27e606e04966913c527b65bf21b6eab93
Kleydson de Barros Vieira <kbvieira@RDL752673.raiadrogasil.com>
2020-11-12 14:14:25 -0300

    backend na nova maquina

--- app/Models/CartaoCredito.php
+++ app/Models/CartaoCredito.php
@@ -6,10 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class CartaoCredito extends Model
 {
-    protected $table = 'cartao_credito';
+  protected $table = 'cartao_creditos';
 
-    public function cliente()
-    {
-        return $this->hasOne(Cliente::class);
-    }
+  protected $fillable = [
+    'nome_titular', 'cpf_titular', 'numero_cartao', 'cd_cliente'
+  ];
+
+  public function cliente()
+  {
+    return $this->hasOne(Cliente::class);
+  }
 }
